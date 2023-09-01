afbf39bc446cee74c48d2cfc75e2c0c75531b7ed
谢迪 <15386393537@163.com>
2023-09-01 15:10:16 +0800

    feat: 装饰器模式

---
+++ design-mode/src/main/java/decorator/Beverage.java
@@ -0,0 +1,17 @@
+package decorator;
+
+/**
+ * @author 谢迪
+ * Date 2023年09月01日 13:58:00
+ * Description
+ * @Version: v1.0
+ */
+public abstract class Beverage {
+
+    abstract void description();
+
+    abstract void getDescription();
+
+    abstract void cost();
+
+}
---
+++ design-mode/src/main/java/decorator/CondimentDecorator.java
@@ -0,0 +1,23 @@
+package decorator;
+
+/**
+ * @author 谢迪
+ * Date 2023年09月01日 14:52:00
+ * Description
+ * @Version: v1.0
+ */
+public class CondimentDecorator extends Beverage{
+    @Override
+    void description() {
+
+    }
+
+    @Override
+    void getDescription() {
+        System.out.println("");
+    }
+
+    @Override
+    void cost() {
+    }
+}
