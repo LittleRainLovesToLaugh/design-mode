c7240ce2e35d7e7f7abb98f9e141cbe72447c280
谢迪 <15386393537@163.com>
2023-09-07 10:50:38 +0800

    feat: 装饰器模式

--- design-mode/src/main/java/decorator/Soy.java
+++ design-mode/src/main/java/decorator/Soy.java
@@ -2,10 +2,31 @@ package decorator;
 
 /**
  * @ClassnameSoy
- * @Description   Whip
+ * @Description   (装饰器模式：调料具体装饰者)
  * @Version1.0.0
  * @Date2023/9/1 23:29
  * @author小雨 27662
  */
-public class Soy {
+public class Soy extends CondimentDecorator{
+
+    /**
+     * 用一个实例变量记录饮料，也就是被装饰者
+     */
+    Beverage beverage;
+
+    public Soy(Beverage beverage) {
+        this.beverage = beverage;
+    }
+
+    @Override
+    public String getDescription() {
+        return beverage.getDescription() + ", 豆浆";
+    }
+
+    @Override
+    public double cost() {
+        // 要计算带Mocha饮料的价钱。首先把调用委托给被装饰对象，以计算价钱，然后再加上Mocha的价钱，得到最后结果。
+        return .2 + beverage.cost();
+    }
+
 }
--- design-mode/src/main/java/decorator/Whip.java
+++ design-mode/src/main/java/decorator/Whip.java
@@ -2,13 +2,30 @@ package decorator;
 
 /**
  * @Classname Whip
- * @Description TODO
+ * @Description (装饰器模式：调料具体装饰者)
  * @Version1.0.0
  * @Date 2023/9/1 23:31
  * @author小雨 27662
  */
-public class Whip {
+public class Whip extends CondimentDecorator{
 
 
+    /**
+     * 用一个实例变量记录饮料，也就是被装饰者
+     */
+    Beverage beverage;
 
+    public Whip(Beverage beverage) {
+        this.beverage = beverage;
+    }
+
+    @Override
+    public String getDescription() {
+        return beverage.getDescription() + ", 奶泡";
+    }
+
+    @Override
+    public double cost() {
+        return .1 + beverage.cost();
+    }
 }
